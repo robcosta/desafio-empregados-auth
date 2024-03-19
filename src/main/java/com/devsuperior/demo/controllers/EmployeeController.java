1dde9c0bd47c02823998b181b1f57bf90e4a8948
Roberto Costa <robertocosta66@gmail.com>
2024-03-18 22:28:26 -0300

    EmployeeControllerIT

--- src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
+++ src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.demo.dto.EmployeeDTO;
 import com.devsuperior.demo.services.EmployeeService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/employees")
@@ -32,8 +35,9 @@ public class EmployeeController {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
-	public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO dto) {
+	public ResponseEntity<EmployeeDTO> insert(@Valid @RequestBody EmployeeDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
